1ffbbbd59a2cc3ef75ceb0e9dec718c25f377e90
Jason <jimanproductions@gmail.com>
2021-10-17 21:01:05 +0200

    added config files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
+
+const config = require('./config.json');
+
+
 
 class App extends React.Component {
 
-    onSearchSubmit(term){
-        console.log(term);
+
+    async onSearchSubmit(term){
+        // console.log(term);
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term},
+            headers:{
+                Authorization: `Client-ID ${config.nokkel}`
+            }
+        });
+        // .then((response) =>{
+        //     console.log(response.data.results);
+        // });
+
+        console.log(response.data.results);
     }
+
+
 
     render(){
         return (
